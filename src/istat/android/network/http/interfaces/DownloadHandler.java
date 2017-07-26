0367f426e9ebfb15833c4322132200bfc0d4e071
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2017-07-26 15:22:18 +0000

    v.2.5-beta2-merge-stream

--- src/istat/android/network/http/interfaces/DownloadHandler.java
+++ src/istat/android/network/http/interfaces/DownloadHandler.java
@@ -3,8 +3,6 @@ package istat.android.network.http.interfaces;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 
-import istat.android.network.http.HttpAsyncQuery;
-
 /**
  * Created by istat on 05/11/16.
  */
