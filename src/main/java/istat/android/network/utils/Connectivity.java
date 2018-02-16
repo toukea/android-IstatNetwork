2bf6ad48f62bd36e24ff932564e098a0d9922721
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2018-02-16 10:46:33 +0000

    added, getMacAddress

--- src/main/java/istat/android/network/utils/Connectivity.java
+++ src/main/java/istat/android/network/utils/Connectivity.java
@@ -1,14 +1,19 @@
 package istat.android.network.utils;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkInfo.DetailedState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.telephony.TelephonyManager;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.ArrayList;
@@ -247,6 +252,60 @@ public final class Connectivity {
         } catch (Exception ex) {
         } // for now eat exceptions
         return ips;
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public static String getMacAddress() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toHexString(b & 0xFF) + ":");
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < macBytes.length; i++) {
+                    sb.append(String.format("%02X%s", macBytes[i], (i < macBytes.length - 1) ? "-" : ""));
+                }
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            //handle exception
+        }
+        return getSystemMacAddress();
+    }
+
+    private static String loadFileAsString(String filePath) throws java.io.IOException {
+        StringBuffer data = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
+            String readData = String.valueOf(buf, 0, numRead);
+            data.append(readData);
+        }
+        reader.close();
+        return data.toString();
+    }
+
+    public static String getSystemMacAddress() {
+        try {
+            return loadFileAsString("/sys/class/net/eth0/address")
+                    .toUpperCase().substring(0, 17);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "02:00:00:00:00:00";
+        }
     }
 
 }
